1954411c0598f0d48d27270f7dd9d805361e4a9e
Fernando Zanutto <15229294+fzanutto@users.noreply.github.com>
2024-09-07 15:58:44 -0300

    posting frames and deleting

--- index.ts
+++ index.ts
@@ -9,27 +9,56 @@ const session = new CredentialSession(new URL("https://bsky.social"));
 
 const agent = new Agent(session);
 
-async function main() {
-  const file = fs.readFileSync(path.join(__dirname, "image.png"));
+async function getFileData() {
+  const frames = fs.readdirSync(path.join(__dirname, "frames"));
+  const nextFrame = frames[0];
+  const lastFrame = frames[frames.length - 1];
+
+  const season = nextFrame.slice(1, 3);
+  const episode = nextFrame.slice(4, 6);
+  const currentFrameNumber = parseInt(nextFrame.slice(7, 11));
+  const totalFrames = lastFrame.slice(7, 11);
+
+  const file = fs.readFileSync(path.join(__dirname, "frames", nextFrame));
 
   const blob = new Blob([file.buffer]);
+  return { season, episode, currentFrameNumber, totalFrames, blob, nextFrame };
+}
 
+async function login() {
   await session.login({
     identifier: process.env.BLUESKY_USERNAME!,
     password: process.env.BLUESKY_PASSWORD!,
   });
+}
+
+async function main() {
+  const [fileData, _loginResult] = await Promise.all([getFileData(), login()]);
+
+  const { season, episode, currentFrameNumber, totalFrames, blob, nextFrame } =
+    fileData;
 
   const response = await agent.uploadBlob(blob);
-  console.log(response);
-  const result = await agent.post({
-    text: "Teste",
+
+  const postText = `Temporada ${season} Episódio ${episode} - Frame ${currentFrameNumber} de ${totalFrames}`;
+
+  await agent.post({
+    text: postText,
     embed: {
-      images: [{ image: response.data.blob, alt: "Teste", $type: "app.bsky.embed.images#image" }],
-      $type: "app.bsky.embed.images"
+      images: [
+        {
+          image: response.data.blob,
+          alt: postText,
+          $type: "app.bsky.embed.images#image",
+        },
+      ],
+      $type: "app.bsky.embed.images",
     },
   });
 
-  console.log(result)
+  fs.unlinkSync(path.join(__dirname, "frames", nextFrame));
+
+  console.log(postText);
 }
 
 main();
