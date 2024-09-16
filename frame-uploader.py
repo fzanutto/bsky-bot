25e3a60865bd9751a33fcd7c3cbf71e8399506bd
Fernando Zanutto <15229294+fzanutto@users.noreply.github.com>
2024-09-16 18:43:41 -0300

    logs

--- frame-uploader.py
+++ frame-uploader.py
@@ -5,9 +5,11 @@ def main():
 
     for file in files:
         gcloud_command = f"gcloud compute scp ./frames/{file} bot-grande-familia:/home/nandoaqws/bsky-bot/frames"
+        print("Uploading file: ", file)
         os.system(gcloud_command)
+        print("Removing file: ", file)
         os.remove("./frames/" + file)
-
+        
 
 if __name__ == "__main__":
     main()
